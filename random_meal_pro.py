f22c335412fc06060205fdadaebce94915a87ede
Henrry168 <1319058861@qq.com>
2021-10-16 13:15:19 +0800

    Update random_meal_pro.py

--- random_meal_pro.py
+++ random_meal_pro.py
@@ -12,11 +12,11 @@ time_interval_1 = Interval("7:00:00", "9:00:00")
 time_interval_2 = Interval("11:00:00", "13:00:00")
 time_interval_3 = Interval("17:00:00", "20:00:00")
 if now_time in time_interval_1:
-    meal = '早'
+    meal = '早上'
 elif now_time in time_interval_2:
-    meal = '午'
+    meal = '中午'
 else:
-    meal = '晚'
+    meal = '晚上'
 
 def clone(self):
     result = []
@@ -35,17 +35,17 @@ def shuffle(lis):
     return result
         
 西苑的大宝贝 = {
-        '早': {1: random.choice(shuffle(clone([1,2,3,4,5,6,7,8,9])))},
+        '早上': {1: random.choice(shuffle(clone([1,2,3,4,5,6,7,8,9])))},
         
-        '午': {
+        '中午': {
                1: random.choice(shuffle(clone([1,2,5,7,8,9,12,17]))),
-               2: random.choice(shuffle(clone([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25]))),
+               2: random.choice(shuffle(clone(['A01', 'A02', 'A03', 'A04', 'A05', 'A06', 'A07', 'A08', 'A09', 'A10', 'A11', 'A12', 'A13', 'B01', 'B02', 'B03', 'B04', 'B05', 'B06', 'B07', 'B08', 'B09', 'B10', 'B11', 'B12', 'B13']))),
                3: random.choice(shuffle(clone([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25])))
               },
         
-        '晚':  {
+        '晚上':  {
                1: random.choice(shuffle(clone([1,2,5,7]))),
-               2: random.choice(shuffle(clone([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25]))),
+               2: random.choice(shuffle(clone(['A01', 'A02', 'A03', 'A04', 'A05', 'A06', 'A07', 'A08', 'A09', 'A10', 'A11', 'A12', 'A13', 'B01', 'B02', 'B03', 'B04', 'B05', 'B06', 'B07', 'B08', 'B09', 'B10', 'B11', 'B12', 'B13']))),
                3: random.choice(shuffle(clone([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25])))
               },
         
@@ -57,19 +57,19 @@ def shuffle(lis):
        }
 
 东苑的大宝贝 = {
-        '早': {
+        '早上': {
                1: random.choice(shuffle(clone([1,2,3,4,5,6,7,8,9]))),
                2: random.choice(shuffle(clone([1,2,3,4,5,6,7,8,9,12,23,24]))),
                3: random.choice(shuffle(clone([1,3,5,7,9,11,13])))
                },
         
-        '午': {
+        '中午': {
                1: random.choice(shuffle(clone([1,2,5,7,8,9,12,17]))),
                2: random.choice(shuffle(clone([1,2,3,4,5,6,7,8,9,12,23,24]))),
                3: random.choice(shuffle(clone([1,3,5,7,9,11,13])))
               },
         
-        '晚':  {
+        '晚上':  {
                1: random.choice(shuffle(clone([1,2,5,7]))),
                2: random.choice(shuffle(clone([1,2,3,4,5,6,7,8,9,12,23,24]))),
                3: random.choice(shuffle(clone([1,3,5,7,9,11,13])))
@@ -135,7 +135,7 @@ def 最终文本():
     吃什么好呢(meal)
     微博()
     时间 = '现在时刻，北京时间' + str(now_time)
-    问候 = '大家好，今天' + meal + '上' + '的幸运窗口是：'
+    问候 = '大家好，今天' + meal + '的幸运窗口是：'
     文本 = 时间 + '\n\n' + 问候 + 去西苑 + '\n' + '来看看这里有什么吧: ' + str(西苑微博) + '\n' + '\n' + '您还可以选择：东苑' + '\n' + '来看看这里有什么吧: 哎呀！这里什么也没有，真不好意思呢。' + '\n\n' + '请收下食堂菌的一言，不要介意哟：' + '\n' + content_yiyan
       
 最终文本()
