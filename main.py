c673fa0c81e0357195ca732fecb76edbbb23a4b3
Nexmoe <16796652+nexmoe@users.noreply.github.com>
2021-10-18 17:16:29 +0800

    Update main.py

--- main.py
+++ main.py
@@ -49,28 +49,24 @@ def 微博():
 
 
 def 一言():
-    global content_yiyan
     headers = {
         'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36 Edg/93.0.961.52'
     }
     res = requests.get('https://v1.hitokoto.cn/', headers=headers)
     saying = res.json()["hitokoto"]
     writer = res.json()["from"]
-    content_yiyan = saying + '\n' + \
+    return saying + '\n' + \
         '                                                                             ————' + writer
 
 
 def 最终文本():
     global 文本
-    一言()
     吃什么好呢(meal)
     微博()
     时间 = '现在时刻，北京时间' + str(now_time)
     问候 = '大家好，今天' + meal + '的幸运窗口是：'
-    文本 = 时间 + '\n\n' + 问候 + 去西苑 + '\n' + '来看看这里有什么吧: ' + \
+    return 时间 + '\n\n' + 问候 + 去西苑 + '\n' + '来看看这里有什么吧: ' + \
         str(西苑微博) + '\n' + '\n' + '您还可以选择：东苑' + '\n' + '（东苑正在完善，别催了，555 o(╥﹏╥)o）' + \
-        '\n\n' + '请收下食堂菌的一言，不要介意哟：' + '\n' + content_yiyan
+        '\n\n' + '请收下食堂菌的一言，不要介意哟：' + '\n' + 一言()
 
-
-最终文本()
-bot.dingtalk(文本)
+bot.dingtalk(最终文本())
