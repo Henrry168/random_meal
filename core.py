588f84447ffa283a34eccc9d0062b4b6a174ec55
Nexmoe <16796652+nexmoe@users.noreply.github.com>
2021-10-18 16:41:46 +0800

    整理

---
+++ core.py
@@ -0,0 +1,17 @@
+import random
+
+def clone(self):
+    result = []
+    for i in range(1000):
+        for x in self:
+            result.append(x)
+    
+    return result
+
+def shuffle(lis):
+    result = []
+    while lis:
+        p = random.randrange(0, len(lis))
+        result.append(lis[p])
+        lis.pop(p)
+    return result
