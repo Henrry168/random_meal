cc888b38e2f6b762d990c1bd839dc4dcfd9c6318
Nexmoe <16796652+nexmoe@users.noreply.github.com>
2021-10-22 18:03:33 +0800

    Update qq.py

--- qq.py
+++ qq.py
@@ -1,6 +1,4 @@
 from mirai import Mirai, WebSocketAdapter, FriendMessage, Startup, Shutdown
-#from apscheduler.schedulers.asyncio import AsyncIOScheduler
-#from apscheduler.trigger.cron import CronTrigger
 
 import main
 
@@ -10,22 +8,10 @@ bot = Mirai(
         verify_key='233123', host='localhost', port=8080
     )
 )
-"""
-scheduler = AsyncIOScheduler()
-
 @bot.on(Startup)
-def start_scheduler(_):
-    scheduler.start() # 启动定时器
-
-@bot.on(Shutdown)
-def stop_scheduler(_):
-    scheduler.shutdown(True) # 结束定时器
-
-@scheduler.scheduled_job(CronTrigger(hour=17, minute=36))
-async def timer():
+async def start_scheduler(_):
     await bot.send_group_message(1021229748,  main.最终文本())
-"""
-
+        
 @bot.on(FriendMessage)
 async def on_friend_message(event: FriendMessage):
     if str(event.message_chain) == '吃点啥':
